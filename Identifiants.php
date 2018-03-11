fd1694c3b2f7a777648af883eb563abacd0ca92c
Benjamin Talmant <32191733+Altefcatre@users.noreply.github.com>
2018-03-11 17:53:16 +0100

    Add files via upload

--- Identifiants.php
+++ Identifiants.php
@@ -1,38 +1,34 @@
 <html>
 	<head>
-	<meta http-equiv="Content-Type" content="text/html"; charset="utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
-	<title>Reservation cantine</title>
-    <meta name="author" content="Talmant Benjamin" />
-	<link href="CSS_Identifiants.css" rel="stylesheet" type="text/css"/>
+		<meta http-equiv="Content-Type" content="text/html"; charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<title>Reservation cantine</title>
+		<meta name="author" content="Talmant Benjamin" />
+		<link href="CSS_Identifiants.css" rel="stylesheet" type="text/css"/>
 	</head> 	
-<body>
-<FORM name="formulaire" METHOD="POST" action="Verfication.php" style ="margin-left:5px;">
-<center><table id="B1" style="margin-top:200px;height:250px;width:500px;border-width:0.5px;"></center>
-<thead>
-</thead>
-<tbody>
-<tr>
-<td style="font-size:18px;">
-<br><b>Identifiant : </b><input placeholder="Identifiant" type="text" 
-style="margin-left:20px;margin-right:5%" size=20 name="Id" value="" required></br>
-</td>
-</tr>
-<tr>
-<td style="font-size:18px;">
-<br><b>Mot de passe :</b><input placeholder="Mot de passe" type="password" 
-style="margin-left:20px;margin-right:5%" size=20 name="Code" value="" required></br>
-</td>
-</tr>
-<tr>
-<td>
-<input id="bouton" align="button" class="btn-primary" type="submit" 
-style ="width:100px;margin-left:200px;margin-right:auto;display:block;margin-top:20px;margin-bottom:5%" value="Se connecter">
-</td>
-</tr>
-</tbody>
-</table>
-</FORM>
+	<body>
+		<FORM name="formulaire" METHOD="POST" action="Verfication.php" style ="margin-left:5px;">
+			<center><table id="B1" style="margin-top:200px;height:250px;width:500px;border-width:0.5px;"></center>
+			<tr>
+			<td style="font-size:18px;">
+				<br><b>Identifiant : </b><input placeholder="Identifiant" type="text" 
+				style="margin-left:20px;margin-right:5%" size=20 name="Id" value="" required></br>
+			</td>
+			</tr>
+			<tr>
+			<td style="font-size:18px;">
+				<br><b>Mot de passe :</b><input placeholder="Mot de passe" type="password" 
+				style="margin-left:20px;margin-right:5%" size=20 name="Code" value="" required></br>
+			</td>
+			</tr>
+			<tr>
+			<td>
+				<input id="bouton" align="button" class="btn-primary" type="submit" 
+				style ="width:100px;margin-left:200px;margin-right:auto;display:block;margin-top:20px;margin-bottom:5%" value="Se connecter">
+			</td>
+			</tr>
+			</table>
+		</FORM>
 
 	</body>
 </html>
---
+++ InfosCompte.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html"; charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+	<title>Reservation cantine</title>
+    <meta name="author" content="Talmant Benjamin" />
+	<link href="CSS_InfosCompte.css" rel="stylesheet" type="text/css"/>
+	</head> 	
+<body>
+	<center><table id="B1" style="margin-top:50px;height:200px;width:500px;border-width:0.5px;">
+			<tr>
+			<td>
+				<br>Statut :</br>
+			</td>
+			</tr>
+			<tr>
+			<td>
+				<br>Solde du compte :</br>
+			</td>
+			</tr>
+	</table></center>
+	<center><table id="B1" style="margin-top:50px;height:500px;width:500px;border-width:0.5px;">
+			<tr>
+			<td>
+				<br>Historique des debits :</br>
+			</td>
+			</tr>
+	</table></center>
+	<a href="Accueil.html"><input class="btn-retour" type="button" value="Retour"></a>
+</body>
+</html>
